502a355a4b44c8648ac480e92af40ae5f2b94f3f
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2023-03-10 09:57:08 +0100

    improve contrast

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -6,7 +6,7 @@ interface ILayoutProps {
 
 const Layout = ({ children }: ILayoutProps) => {
   return (
-    <div className='bg-green-500 flex flex-col min-h-screen'>
+    <div className='bg-green-200 flex flex-col min-h-screen'>
       <header className="bg-slate-800 text-white text-center w-full">Header</header>
       <main className="grow px-8 lg:px-24 2xl:px-32 3xl:w-9/12 m-auto max-w-screen-3xl">
         {children}
