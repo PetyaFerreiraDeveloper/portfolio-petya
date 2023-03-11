2df9695cc980708e28779bff144bca2ac0df7682
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2023-03-11 09:17:48 +0100

    toggle dark mode

--- components/common/header/DarkModeToggle.tsx
+++ components/common/header/DarkModeToggle.tsx
@@ -1,13 +1,35 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import SunIcon from "@/components/svgs/SunIcon";
 import MoonIcon from "@/components/svgs/MoonIcon";
 
 const DarkModeToggle = () => {
+  let [day, setDay] = useState(false);
+
+  let mode;
+
+  // if (day) {
+  //   mode = `animation-goRight`;
+  // } else {
+  //   mode = `animation-goLeft`;
+  // }
+
+  if (day) {
+    mode = `right-0`;
+  } else {
+    mode = `left-0`;
+  }
+
   return (
     <div className="flex items-center gap-4 bg-gray-500 rounded-xl relative">
-      <div className="w-6 h-6 bg-red-500 rounded-full absolute"></div>
-      <SunIcon width={"25"} height={"25"} fill={"white"} />
-      <MoonIcon width={"25"} height={"25"} fill={"white"} />
+      <div
+        className={`w-6 h-6 bg-red-500 rounded-full absolute ${mode} `} //absolute transition ease-in-out delay-150 
+      ></div>
+      <button onClick={() => setDay(false)} className="" aria-label="">
+        <SunIcon width={"25"} height={"25"} fill={"white"} />
+      </button>
+      <button onClick={() => setDay(true)} className="0" aria-label="">
+        <MoonIcon width={"25"} height={"25"} fill={"white"} />
+      </button>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,28 @@ module.exports = {
     "./layout/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // keyframes: {
+      //   goRight: {
+      //     '0%:': { transform: 'transitionX(0)' },
+      //     '25%': { transform: 'transitionX(25%)' },
+      //     '50%': { transform: 'transitionX(50%)' },
+      //     '75%': { transform: 'transitionX(75%)' },
+      //     '100%': { transform: 'transitionX(100%)' },
+      //   },
+      //   goLeft: {
+      //     '0%:': { transform: 'transitionX(0%)' },
+      //     '25%': { transform: 'transitionX(-25%)' },
+      //     '50%': { transform: 'transitionX(-50%)' },
+      //     '75%': { transform: 'transitionX(-75%)' },
+      //     '100%': { transform: 'transitionX(-100%)' },
+      //   }
+      // },
+      // animation: {
+      //   goRight: 'goRight 1s linear',
+      //   goLeft: 'goLeft 1s linear',
+      // }
+    },
   },
   plugins: [],
 }
