3c0cd66e69d0efdf1da7d0e1a4cc0249bc5e5903
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2023-03-13 19:42:20 +0100

    hero

---
+++ components/common/hero/Hero.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section>
+      <div>
+
+      <span>Hello, my name is</span>
+      <h2>Petya Antonova Naydenova Ferreira</h2>
+      </div>
+      <p>I am a fullstack developer</p>
+
+    </section>
+  )
+}
+
+export default Hero;
